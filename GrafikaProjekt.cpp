6cf8cdfea449c485d2ce75330f843bcf2f0330d1
glamac <183738326+glamac@users.noreply.github.com>
2025-04-27 19:07:55 +0200

    Setup projektu.

---
+++ GrafikaProjekt.cpp
@@ -0,0 +1,12 @@
+﻿// GrafikaProjekt.cpp : Defines the entry point for the application.
+//
+
+#include "GrafikaProjekt.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
